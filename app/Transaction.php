7ac32a80964188547eb4cd0b4d9e3b57b9e4833b
Rhioda muthie <rhiodam@gmail.com>
2019-04-30 11:20:04 +0700

    1 to many done

---
+++ app/Transaction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    //
+    public function employee()
+    {
+        return $this->belongsTo('App\Employee');
+    }
+
+}
